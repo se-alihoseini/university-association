c05e0793ba0cb377328a27e6ee07cf8377f56a4f
se-alihoseini <se.alihoseini@gmail.com>
2023-05-17 22:04:51 +0430

    settings commit

--- university_association/settings.py
+++ university_association/settings.py
@@ -180,16 +180,16 @@ SPECTACULAR_SETTINGS = {
 }
 
 
-# storage
-AWS_S3_ENDPOINT_URL = os.environ.get('LIARA_ENDPOINT')
-AWS_STORAGE_BUCKET_NAME = os.environ.get('LIARA_BUCKET_NAME')
-AWS_ACCESS_KEY_ID = os.environ.get('LIARA_ACCESS_KEY')
-AWS_SECRET_ACCESS_KEY = os.environ.get('LIARA_SECRET_KEY')
-AWS_S3_OBJECT_PARAMETERS = {
-  'CacheControl': 'max-age=86400',
-}
-AWS_LOCATION = 'static'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# # storage
+# AWS_S3_ENDPOINT_URL = os.environ.get('LIARA_ENDPOINT')
+# AWS_STORAGE_BUCKET_NAME = os.environ.get('LIARA_BUCKET_NAME')
+# AWS_ACCESS_KEY_ID = os.environ.get('LIARA_ACCESS_KEY')
+# AWS_SECRET_ACCESS_KEY = os.environ.get('LIARA_SECRET_KEY')
+# AWS_S3_OBJECT_PARAMETERS = {
+#   'CacheControl': 'max-age=86400',
+# }
+# AWS_LOCATION = 'static'
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
 CKEDITOR_CONFIGS = {
