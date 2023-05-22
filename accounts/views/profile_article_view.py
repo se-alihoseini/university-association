3d19dc3aabbc7c703ab8a7354f918c203d43679f
se-alihoseini <se.alihoseini@gmail.com>
2023-05-22 06:52:48 +0430

    Remove unused code

--- accounts/views/profile_article_view.py
+++ accounts/views/profile_article_view.py
@@ -2,7 +2,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 from post.models import Article
-from home.serializer import HomeArticleSerializer
+from post.serializer import ArchiveArticleSerializer
 
 
 class ArticleProfileView(APIView):
@@ -10,5 +10,5 @@ class ArticleProfileView(APIView):
     def get(self, request):
         user = request.user
         queryset = Article.objects.filter(author=user)
-        srz_data = HomeArticleSerializer(data=queryset, many=True)
+        srz_data = ArchiveArticleSerializer(data=queryset, many=True)
         return Response(data=srz_data.data, status=status.HTTP_200_OK)
