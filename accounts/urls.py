e577ccef51d97efc34d32efc6e504e5d0e23f38c
se-alihoseini <se.alihoseini@gmail.com>
2023-05-27 15:15:23 +0430

    token validation view

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from accounts.views import register_view, login_view, logout_view, forgot_password_view, profile_article_view, \
-    user_update, user_profile
+    user_update, user_profile, token_validation_view
 
 app_name = 'accounts'
 urlpatterns = [
@@ -12,4 +12,5 @@ urlpatterns = [
     path('articles/', profile_article_view.ArticleProfileView.as_view(), name='profile_article'),
     path('user/update/', user_update.UserUpdate.as_view(), name='user_update'),
     path('user/profile/', user_profile.UserProfileView.as_view(), name='user_profile'),
+    path('token_validation/', token_validation_view.TokenValidationView.as_view(), name='token_validation'),
 ]
---
+++ accounts/views/token_validation_view.py
@@ -0,0 +1,28 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from datetime import datetime
+import jwt
+from django.conf import settings
+
+
+class TokenValidationView(APIView):
+
+    def get(self, request):
+        token = request.META.get('HTTP_AUTHORIZATION', '').split(' ')[1]
+
+        try:
+            decoded_token = jwt.decode(token, settings.SECRET_KEY, algorithms=['HS256'])
+            expiration_time = decoded_token['exp']
+            current_time = int(datetime.now().timestamp())
+
+            if expiration_time < current_time:
+                return Response({'message': 'Token expired'}, status=status.HTTP_401_UNAUTHORIZED)
+            else:
+                return Response({'message': 'Token is valid'}, status=status.HTTP_200_OK)
+
+        except jwt.ExpiredSignatureError:
+            return Response({'message': 'Token expired'}, status=status.HTTP_401_UNAUTHORIZED)
+
+        except jwt.InvalidTokenError:
+            return Response({'message': 'Invalid token'}, status=status.HTTP_401_UNAUTHORIZED)
