50f2c17b5c96678d97f51552cff8579240d48351
se-alihoseini <se.alihoseini@gmail.com>
2023-05-28 15:45:07 +0430

    debug commit

--- course/urls.py
+++ course/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('teachers/', teacher_list_view.TeacherListView.as_view(), name='list_teachers_view'),
     path('courses/', course_list_view.CourseListView.as_view(), name='list_teachers_view'),
     path('<slug:slug>/', course_retrieve_view.CourseRetrieveView.as_view(), name='course_retrieve_view'),
-    path('<slug:course_video_slug>/', course_video_retrieve_view.CourseVideoRetrieveView.as_view(),
+    path('<slug:course_video_slug>', course_video_retrieve_view.CourseVideoRetrieveView.as_view(),
          name='course_video_retrieve_view'),
     path('teacher/<slug:teacher_slug>/', teacher_courses.TeacherCoursesView.as_view(), name='teacher_courses_view'),
 ]
