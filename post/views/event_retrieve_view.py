cfdb95f9cdd848eaa7433a8b3cfc1d590fe1ea37
se-alihoseini <se.alihoseini@gmail.com>
2023-06-02 21:56:08 +0430

    event update

--- post/views/event_retrieve_view.py
+++ post/views/event_retrieve_view.py
@@ -6,4 +6,5 @@ from post.serializer import RetrieveEventSerializer
 class EventRetrieveView(RetrieveAPIView):
     serializer_class = RetrieveEventSerializer
     authentication_classes = []
+    lookup_field = 'slug'
     queryset = Event.objects.all()
